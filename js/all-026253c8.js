ae4c9fe4f0073f3319e1c53a839a07a1eec486b6
Benoit Rajalu <rajalubenoit@gmail.com>
2014-09-12 11:52:21 +0200

    Automated commit at 2014-09-12 09:52:21 UTC by middleman-deploy 0.3.0

--- js/all-026253c8.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(window).load(function(){$("#mainSlider").flexslider({controlNav:!0,smoothHeight:!0,animation:"slide",pauseOnHover:!0,manualControls:"#sliderNav li a"}),function(){var e=$("#threeLines");e.on("click",function(){$(this).toggleClass("active"),$("#topNav").toggleClass("open")})}()});
