7cd35fb17ce31b0b2e551c1859ae780d83857da3
Benoit Rajalu <rajalubenoit@gmail.com>
2014-09-16 15:40:04 +0200

    Latency on tooltips

--- source/js/tooltip.js
+++ source/js/tooltip.js
@@ -3,7 +3,8 @@ $( function()
     var targets = $( '[rel~=tooltip]' ),
         target  = false,
         tooltip = false,
-        title   = false;
+        title   = false,
+		timer;
  
     targets.bind( 'mouseenter', function()
     {
@@ -21,73 +22,81 @@ $( function()
  
         var init_tooltip = function()
         {
-			if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-                tooltip.css( 'max-width', $( window ).width() / 2 );
-            else
-                tooltip.css( 'max-width', 340 );
-
-			if(target.hasClass("left")){
-				var pos_left = target.offset().left + target.outerWidth() + 15,
-		            pos_top  = target.offset().top - (tooltip.outerHeight()/2) -2;
-					tooltip.addClass( 'leftMost' );
-	 
-	            if( pos_left + tooltip.outerWidth() > $( window ).width() )
-	            {
-	                var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-	                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
-					tooltip.removeClass( 'leftMost' );
-	            }
-
-				if( pos_left + tooltip.outerWidth() > $( window ).width() )
-	            {
-	                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-	                tooltip.addClass( 'right' );
-	            }
-	            else
-	                tooltip.removeClass( 'right' );
-	 
-	            if( pos_top < 0 )
-	            {
-	                var pos_top  = target.offset().top + target.outerHeight();
-	                tooltip.addClass( 'top' );
-	            }
-	            else
-	                tooltip.removeClass( 'top' );
-	 
-	            tooltip.css( { left: pos_left, top: pos_top } )
-                   .animate( { top: '+=10', opacity: 1 }, 50 );
+			if(timer) {
+				clearTimeout(timer);
+				timer = null
 			}
-			else{	 
-	            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-	                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
-	 
-	            if( pos_left < 0 )
-	            {
-	                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-	                tooltip.addClass( 'left' );
-	            }
+
+			timer = setTimeout(function() {
+
+				if( $( window ).width() < tooltip.outerWidth() * 1.5 )
+	                tooltip.css( 'max-width', $( window ).width() / 2 );
 	            else
-	                tooltip.removeClass( 'left' );
-	 
-	            if( pos_left + tooltip.outerWidth() > $( window ).width() )
-	            {
-	                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-	                tooltip.addClass( 'right' );
-	            }
-	            else
-	                tooltip.removeClass( 'right' );
-	 
-	            if( pos_top < 0 )
-	            {
-	                var pos_top  = target.offset().top + target.outerHeight();
-	                tooltip.addClass( 'top' );
-	            }
-	            else
-	                tooltip.removeClass( 'top' );
-	 
-	            tooltip.css( { left: pos_left, top: pos_top } )
-                   .animate( { top: '+=10', opacity: 1 }, 50 );
-			}
+	                tooltip.css( 'max-width', 340 );
+
+				if(target.hasClass("left")){
+					var pos_left = target.offset().left + target.outerWidth() + 15,
+			            pos_top  = target.offset().top - (tooltip.outerHeight()/2) -2;
+						tooltip.addClass( 'leftMost' );
+		 
+		            if( pos_left + tooltip.outerWidth() > $( window ).width() )
+		            {
+		                var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+		                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
+						tooltip.removeClass( 'leftMost' );
+		            }
+
+					if( pos_left + tooltip.outerWidth() > $( window ).width() )
+		            {
+		                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+		                tooltip.addClass( 'right' );
+		            }
+		            else
+		                tooltip.removeClass( 'right' );
+		 
+		            if( pos_top < 0 )
+		            {
+		                var pos_top  = target.offset().top + target.outerHeight();
+		                tooltip.addClass( 'top' );
+		            }
+		            else
+		                tooltip.removeClass( 'top' );
+		 
+		            tooltip.css( { left: pos_left, top: pos_top } )
+	                   .animate( { top: '+=10', opacity: 1 }, 50 );
+				}
+				else{	 
+		            var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+		                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
+		 
+		            if( pos_left < 0 )
+		            {
+		                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+		                tooltip.addClass( 'left' );
+		            }
+		            else
+		                tooltip.removeClass( 'left' );
+		 
+		            if( pos_left + tooltip.outerWidth() > $( window ).width() )
+		            {
+		                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+		                tooltip.addClass( 'right' );
+		            }
+		            else
+		                tooltip.removeClass( 'right' );
+		 
+		            if( pos_top < 0 )
+		            {
+		                var pos_top  = target.offset().top + target.outerHeight();
+		                tooltip.addClass( 'top' );
+		            }
+		            else
+		                tooltip.removeClass( 'top' );
+		 
+		            tooltip.css( { left: pos_left, top: pos_top } )
+	                   .animate( { top: '+=10', opacity: 1 }, 50 );
+				}
+			}, 400)
         };
  
         init_tooltip();
