c8113943d1cbac8d22040a3602a70c531442aade
Benoit Rajalu <rajalubenoit@gmail.com>
2014-09-17 10:35:28 +0200

    No more loop on slider, less line height on mobile main title

--- source/js/all.js
+++ source/js/all.js
@@ -27,6 +27,7 @@ $(window).load(function () {
 		$("#mainSlider").flexslider({
 			itemMargin: 300,
 			slideshow:true,
+			animationLoop: false,
 			video: true,
 			touch: true,
 			controlNav: true, 
